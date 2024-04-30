82632cb4dc2c640603f1291db1ab7370414617a4
User <User@WINDOWS-89IVMO7>
2024-04-30 16:40:19 +0530

    first commit......

---
+++ src/com/hcl/core/example/Test.java
@@ -0,0 +1,8 @@
+package com.hcl.core.example;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("vijay");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module GitDemoExamples {
+}

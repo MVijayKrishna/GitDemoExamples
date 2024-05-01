34c2b291f0c638cbb2a3de7734adeb1097ecfded
User <User@WINDOWS-89IVMO7>
2024-05-01 13:39:36 +0530

    added the few  codeed

--- src/com/hcl/core/example/ArthDemo.java
+++ src/com/hcl/core/example/ArthDemo.java
@@ -1,14 +1,18 @@
 package com.hcl.core.example;
 
 public class ArthDemo {
-	int getAge() {
-		System.out.println("vijay inside getAGe");
-		return 4;
+	int getAge(int no) {
+		if (no >= 30) {
+			return no;
+		} else {
+			return 0;
+		}
+
 	}
 
 	public static void main(String[] args) {
 		ArthDemo arthDemo = new ArthDemo();
-		System.out.println(arthDemo.getAge());
+		System.out.println(arthDemo.getAge(25));
 
 	}
 
